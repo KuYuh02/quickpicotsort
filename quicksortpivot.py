3782eb25a1df174707683ee5a92f36837ec1a1d6
Martin De Guzman <57105334+KuYuh02@users.noreply.github.com>
2025-01-28 22:46:50 -0800

    Update quicksortpivot.py
    sort

--- quicksortpivot.py
+++ quicksortpivot.py
@@ -3,9 +3,12 @@ import quicksort
 class Submission(quicksort.QSortBase):
     def pivot(self, lo, hi, depth, breadth):        
         middle_index = lo + (hi - lo) // 2
-        values = [self.array[lo], self.array[middle_index], self.array[hi]]
-        
-        # Sort the three values and return the median
-        values.sort()
-        
-        return values[1]
+        a, b, c = self.array[lo], self.array[middle_index], self.array[hi]
+
+        # Find the median manually
+        if (a <= b <= c) or (c <= b <= a):
+            return b
+        elif (b <= a <= c) or (c <= a <= b):
+            return a
+        else:
+            return c
